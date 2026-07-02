f8c7ba90ba47df0916b694eb66887a5e1ee16c0d
Trent <trentstarbine@gmail.com>
2026-07-02 17:18:02 +0000

    create/update ChannelScanningCursor from messages

--- code/jobs/channel_scanning_job.py
+++ code/jobs/channel_scanning_job.py
@@ -2,6 +2,7 @@
 
 import asyncio
 
+from models.database.channel_scanning_cursor import ChannelScanningCursor
 from models.database.member_activity import MemberActivityRecord, MemberActivityStatus
 
 class ChannelScanningJob:
@@ -18,22 +19,23 @@ class ChannelScanningJob:
             await asyncio.sleep(self._config.channel_scanning_config.minutes_between_scans * 60)
 
     async def scan_all_channels(self):
+        # TODO: scan all channels visible to the bot.
         for channel_pruning_config in self._config.channel_pruning_configs:
-            cursor, member_activity_records = await self.scan_channel(channel_pruning_config)
-            self._database.queue_update(cursor)
-            for record in member_activity_records:
-                self._database.queue_update(record)
+            await self.process_new_messages_in_channel(channel_pruning_config.channel_id)
+
+    async def process_new_messages_in_channel(self, channel_id):
+        cursor = await self._database.get_channel_scanning_cursor(channel_id)
+        messages, updated_cursor = await self._scan_channel_from_cursor(channel_id, cursor)
+
+        print("Calculated new cursor: " + str(updated_cursor))
+        db_updates = [] if updated_cursor is None else [updated_cursor]
+        db_updates += self._process_messages(messages)
+
+        for update in db_updates:
+            self._database.queue_update(update)
         await self._database.commit_updates()
 
-    async def scan_channel(self, channel_pruning_config):
-        cursor = await self._database.get_channel_scanning_cursor(channel_pruning_config.channel_id)
-        member_messages = await self._scan_channel_from_cursor(channel_pruning_config.channel_id, cursor)
-        member_activity_records = self._convert_to_member_activity_records(member_messages)
-        print("Calculated records:")
-        for record in member_activity_records:
-            print(record)
-        updated_cursor = self._create_updated_cursor(cursor, member_messages)
-        return (updated_cursor, member_activity_records)
+        print("Committed updates to database.")
 
     async def _scan_channel_from_cursor(self, channel_id, cursor):
         channel = self._discord_client.get_channel(channel_id)
@@ -48,9 +50,19 @@ class ChannelScanningJob:
             messages.append(message)
 
         category = f"{channel.category.name}: " if channel.category else ""
-        print(f"Finished scanning {category}{channel.name} ({channel.id})")
+        print(f"Finished scanning {category}{channel.name} ({channel.id}) for new messages. Found {len(messages)} messages.")
 
-        return messages
+        updated_cursor = self._create_updated_cursor(cursor, messages)
+        return messages, updated_cursor
+
+    def _process_messages(self, messages):
+        member_activity_records = self._convert_to_member_activity_records(messages)
+        print("Calculated member activity records:")
+        for record in member_activity_records:
+            print(record)
+
+        db_updates = member_activity_records
+        return db_updates
 
     def _convert_to_member_activity_records(self, messages):
         record_by_user_id = dict()
@@ -63,8 +75,10 @@ class ChannelScanningJob:
                 record = MemberActivityRecord.create_from_message(message)
             record_by_user_id[uid] = record
 
-        return record_by_user_id.values()
+        return list(record_by_user_id.values())
 
     def _create_updated_cursor(self, old_cursor, messages):
-        # TODO: create updated cursor with last scanned message info
-        pass
+        if old_cursor is None:
+            return ChannelScanningCursor.create_from_latest_messages(messages)
+        else:
+            return old_cursor.replace_with_latest_messages(messages)
--- code/models/database/channel_scanning_cursor.py
+++ code/models/database/channel_scanning_cursor.py
@@ -8,7 +8,32 @@ from .thread_scanning_cursor import ThreadScanningCursor
 
 @dataclass(frozen=True)
 class ChannelScanningCursor:
+    channel_name: str
     channel_id: int
     last_scanned_message_id: int
     last_scanned_message_timestamp: datetime
     thread_scanning_cursors: List[ThreadScanningCursor]
+
+    @staticmethod
+    def create_from_latest_messages(messages):
+        if len(messages) == 0:
+            return None
+        else:
+            last_message = messages[-1]
+            return ChannelScanningCursor(
+                    channel_name=last_message.channel.name,
+                    channel_id=last_message.channel.id,
+                    last_scanned_message_id=last_message.id,
+                    last_scanned_message_timestamp=last_message.created_at,
+                    thread_scanning_cursors = []
+            )
+
+    def replace_with_latest_messages(self, messages):
+        if len(messages) == 0:
+            return self
+        else:
+            last_message = messages[-1]
+            return self.replace(
+                    last_scanned_message_id=last_message.id,
+                    last_scanned_message_timestamp=last_message.created_at
+            )
