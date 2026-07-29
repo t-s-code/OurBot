50986c7694694084ea4c04766a085c9d721f197e
Trent <trentstarbine@gmail.com>
2026-07-29 14:49:43 +0000

    Add /hf off command

--- code/our_bot.py
+++ code/our_bot.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import discord
+from discord import app_commands
 
 from core.database import Database
 from jobs import ChannelScanningJob
@@ -15,6 +16,7 @@ class OurBot:
         self._discord_client = discord.Client(
             intents=self._build_intents()
         )
+        self._command_tree = app_commands.CommandTree(self._discord_client)
 
         self._database = Database(
             discord_client=self._discord_client,
@@ -28,6 +30,7 @@ class OurBot:
         )
 
         self._register_events()
+        self._register_commands()
 
     # -------------------------
     # Discord Setup
@@ -57,6 +60,15 @@ class OurBot:
             print("", file=sys.stderr)
             await self._discord_client.close()
 
+    def _register_commands(self):
+        hf_group = app_commands.Group(name="hf", description="Housekeeping bot commands")
+
+        @hf_group.command(name="off", description="Shuts down the bot")
+        async def off(interaction: discord.Interaction):
+            await interaction.response.send_message("Shutting down...")
+            await self._discord_client.close()
+
+        self._command_tree.add_command(hf_group)
 
     # -------------------------
     # Event Handlers
@@ -65,6 +77,8 @@ class OurBot:
     async def _on_ready(self):
         bot_user = self._discord_client.user
         print(f"Logged in as {bot_user.display_name} ({bot_user.id})")
+
+        await self._command_tree.sync()
 
         await self._database.load()
         channel_scanning_task = asyncio.create_task(self._channel_scanning_job.run())
